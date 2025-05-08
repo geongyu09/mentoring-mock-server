44451dff6b5f8d58cfe728e4d66afad120758a28
박건규 <geongyu09@gmail.com>
2025-05-08 22:45:07 +0900

    cors 허용

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function middleware() {
   // 응답 객체 가져오기
   const response = NextResponse.next();
 
